d7308da51ddbb022b3924dd38fae1973c37e288e
hoefd001 <ferdy.van.den.hoed@hva.nl>
2016-04-17 18:11:46 +0200

    CameraFollow script

---
+++ SecretSword/Assets/Scripts/CameraFollow.cs
@@ -0,0 +1,12 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CameraFollow : MonoBehaviour
+{
+    public Transform Target;
+    
+	void Update ()
+	{
+	    transform.position = new Vector3(Target.position.x, Target.transform.position.y, transform.position.z);
+	}
+}
